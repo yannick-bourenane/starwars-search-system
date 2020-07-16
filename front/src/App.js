b3753008c77e2b0860228b28eab04f6ed12fa7f9
Yannick Bourenane <yannick.bourenane@gmail.com>
2020-07-16 18:47:00 +0200

    link back/front with fetch test

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+const axios = require("axios");
 
 function App() {
+  function testFetchBack() {
+    axios
+      .get(process.env.REACT_APP_BACKEND_URL)
+      .then((res) => console.log("res = ", res));
+  }
+
   return (
     <div className="App">
+      {testFetchBack()}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
